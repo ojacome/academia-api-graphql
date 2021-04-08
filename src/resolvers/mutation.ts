47614be447777eb2845c5aeb09bac2030b6253fe
ojacome01 <ojacome@libelulasoft.com>
2021-04-08 12:27:42 -0500

    modificar curso

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,6 +1,6 @@
 import { IResolvers } from "graphql-tools";
 import { database } from "../data/data.store";
-
+import _ from "lodash";
 
 
 
@@ -21,8 +21,49 @@ const mutation: IResolvers = {
                 reviews:        []
             }
             
-            database.cursos.push(itemCurso);
-            return itemCurso;
+            const cursosRepetidos = database.cursos.filter( itemCur => itemCur.title === itemCurso.title);
+            if( cursosRepetidos.length === 0 ){
+                database.cursos.push(itemCurso);
+                return itemCurso;
+            }
+
+            return {
+                id:             '-1',
+                title:          `el curso ya existe con este titulo`,
+                description:    '',
+                clases:         -1,
+                time:           0.0,
+                logo:           '',
+                level:          'TODOS',
+                path:           '',
+                teacher:        '',
+                reviews:        []
+            }
+        },
+        modificarCurso(__: void, { curso }): any{
+            const existeElemento = _.findIndex(database.cursos, function(o) {
+                return o.id === curso.id
+            })
+
+            if( existeElemento > -1 ){
+                const valoraciones = database.cursos[existeElemento].reviews;
+                curso.reviews = valoraciones;
+                database.cursos[existeElemento] = curso;
+                return curso;
+            }
+
+            return {
+                id:             '-1',
+                title:          `el curso NO existe`,
+                description:    '',
+                clases:         -1,
+                time:           0.0,
+                logo:           '',
+                level:          'TODOS',
+                path:           '',
+                teacher:        '',
+                reviews:        []
+            }
         }        
     }
 }
